df7f2c006d4f14d0dfd46a756d4bdb41455159db
DavidSC <david@wbcboxing.com>
2021-06-08 02:10:18 -0500

    task chhild component

---
+++ src/components/Todo/FieldAddTask.vue
@@ -0,0 +1,36 @@
+<template>
+        <v-text-field
+            class="pa-3"
+           v-model="newTaskTitle"
+           @click:append="addTask"
+           @keyup.enter="addTask"
+            outlined
+            label="Add Task"
+            append-icon="mdi-plus"
+            hide-details
+            clearable
+          ></v-text-field>
+</template>
+
+<script>
+export default {
+ data(){
+      return{
+        newTaskTitle:'',
+       
+      }
+    },
+    methods: {
+      addTask(){
+          
+        this.$store.commit('addTask',this.newTaskTitle);
+        this.newTaskTitle = '';
+      }   
+        
+    }
+}
+</script>
+
+<style>
+
+</style>
--- src/views/Todo.vue
+++ src/views/Todo.vue
@@ -1,99 +1,36 @@
 <template>
   <div class="home ">
-    <v-text-field
-    class="pa-3"
-           v-model="newTaskTitle"
-           @click:append="addTask"
-           @keyup.enter="addTask"
-            outlined
-            label="Add Task"
-            append-icon="mdi-plus"
-            hide-details
-            clearable
-          ></v-text-field>
-    <v-list
-    v-if="tasks.length"
-      class="pt-0"
-      flat  
-    >
-        <div  
-            v-for="task in tasks" :key="task.id"    
-        >
-            <v-list-item 
-             @click="doneTask(task.id)"
-             :class="{'blue lighten-5' :task.done}"
-            >
-              <template v-slot:default>
-                <v-list-item-action>
-                  <v-checkbox :input-value="task.done"></v-checkbox>
-                </v-list-item-action>
-
-                <v-list-item-content>
-                  <v-list-item-title
-                  :class="{'text-decoration-line-through':task.done}"
-                  >
-                  {{task.title}}
-                  </v-list-item-title>              
-                </v-list-item-content>
-               <v-list-item-action>
-                  <v-btn 
-                  @click.stop="deleteTask(task.id)"
-                  icon
-                  >
-                    <v-icon color="primary lighten-1">mdi-delete</v-icon>
-                  </v-btn>
-              </v-list-item-action>
-              </template>
-
-            </v-list-item>
-            <v-divider></v-divider>
-        </div>
-    </v-list>
-    <div
-     v-else
-     class="no-tasks"
-     >
-     <v-icon
-      size="100"
-      color="primary"
+      <field-add-task />
+      <list-tasks
+        v-if="$store.state.tasks.length"
+      />
+      <div
+      v-else
+      class="no-tasks"
       >
-      mdi-check
-    </v-icon>
-     <div class="text-h5 primary--text">No tasks</div>
-    
-    </div>
+            <v-icon
+              size="100"
+              color="primary"
+             >
+             mdi-check
+            </v-icon>
+            <div class="text-h5 primary--text">No tasks</div>
+          
+      </div>
   </div>
 </template>
 
 <script>
-
+import FieldAddTask from "../components/Todo/FieldAddTask";
+import ListTasksVue from '../components/Todo/ListTasks.vue';
 
   export default {
-    name: 'Todo',
-    data(){
-      return{
-        newTaskTitle:'',
-        tasks:[
-          {id:1,title:'Wake Up',done:false},
-          {id:2,title:'Get bananas',done:false},
-          {id:3,title:'Eat bananas',done:false},
-        ]
-      }
-    },
-    methods: {
-      addTask(){
-         let newTask = {id:Date.now(),title:this.newTaskTitle,done:false};
-         this.tasks.push(newTask);
-         this.newTaskTitle=''
-      },
-      doneTask(id) {
-        let task = this.tasks.filter( task => task.id === id)[0];
-        task.done = !task.done;
-      },
-      deleteTask(id){
-        this.tasks = this.tasks.filter( task => task.id !== id);
-      }
-    }, 
+    name: 'Todo',   
+    components:{
+      'field-add-task':FieldAddTask,
+      'list-tasks':ListTasksVue
+
+    }
   }
 </script>
  <style lang="scss">
